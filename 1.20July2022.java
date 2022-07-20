c929d0f965aa41fde19016dbd77b083a24588ae5
Ankit kanyal <85594947+Ankitkanyal1@users.noreply.github.com>
2022-07-20 18:19:46 +0530

    Create 1.20July2022.java

---
+++ 1.20July2022.java
@@ -0,0 +1,23 @@
+// Binary Search Algorithm
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length-1;
+        int first=0;
+        while(first<=n){
+            int mid=(first+n)/2;
+            if(nums[mid]<target){
+                first=mid+1;
+            }
+            else if(nums[mid]==target){
+               return mid;  
+            }
+            else{
+                n=mid-1;
+            }
+            mid=(first+n)/2;
+           
+        }
+        return -1;
+        
+    }
+}
